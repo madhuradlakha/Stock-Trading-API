5a1d53cb97a0e7a6573bdfe3afc20832ecdaaef7
Madhur Adlakha <madhuradlakha@yahoo.co.in>
2016-01-27 00:47:25 +0530

    changes

---
+++ StockTrader/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name="StockTrader",
+	version="1.0",
+	description="It helps one know the value of their stock",
+	url="https://github.com/madhuradlakha/Stock-Trading-API/blob/master/stock.py",
+	author="Madhur Adlakha",
+	author_email="madhuradlakha@yahoo.co.in",
+	license='MIT',
+	packages=["StockTrader"],
+	scripts=["bin/StockTrader"],
+	zip_safe=False)
